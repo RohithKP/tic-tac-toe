89a417e2488bfdf6657f33cb0ef2a39a8fbed522
rki-financial-com <rki@financial.com>
2021-10-30 22:33:46 +0530

    Basic grid layout created using tailwind css

---
+++ src/components/Board.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="grid justify-center items-center content-center grid-cols-3">
+    <Cell v-for="i in 9" :key="i"></Cell>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import Cell from "./Cell.vue";
+
+export default defineComponent({
+  name: "Board",
+  components: {
+    Cell
+  },
+  setup() {}
+});
+</script>
+
+<style scoped>
+.cell {
+  width: var(--cell-size);
+  height: var(--cell-size);
+  border: 1px solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+}
+
+.cell:first-child,
+.cell:nth-child(2),
+.cell:nth-child(3) {
+  border-top: none;
+}
+
+.cell:nth-child(3n + 1) {
+  border-left: none;
+}
+
+.cell:nth-child(3n + 3) {
+  border-right: none;
+}
+
+.cell:last-child,
+.cell:nth-child(8),
+.cell:nth-child(7) {
+  border-bottom: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Board />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from "vue";
+import Board from "@/components/Board.vue";
 
 export default defineComponent({
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld,
-  },
+    Board
+  }
 });
 </script>
